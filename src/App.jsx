0b247e79e29da8cfc3e74f922d899c3b83642d9d
Rahul Kumar <hmko5432@gmail.com>
2026-09-12 21:51:26 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
